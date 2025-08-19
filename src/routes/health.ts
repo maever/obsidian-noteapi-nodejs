389e77a9c055d730ab671c9ae521c55d8316e65b
RobinG <maever@gmail.com>
2025-08-19 21:54:10 +0200

    Updated health.ts route logging (very spammy)

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,5 +2,8 @@ import { FastifyInstance } from 'fastify';
 
 
 export default async function route(app: FastifyInstance) {
-    app.get('/health', async () => ({ ok: true }));
+  app.get('/health', async (request, reply) => {
+    //console.log('[Health] endpoint hit at', new Date().toISOString());
+    return { ok: true };
+  });
 }
